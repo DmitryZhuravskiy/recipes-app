f94ef7c01873bb4cedeb955e00517acd6dc091cd
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2023-06-02 18:12:33 +0300

    v

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { recipesRouter } from "./routes/recipes.js";
-import 'dotenv/config'
+import "dotenv/config";
+//request.setHeader("Content-Security-Policy", "default-src 'self'");
 
 const app = express();
 
@@ -11,20 +12,28 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy-Report-Only",
+    "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+  );
+
+  next();
+});
+
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
 mongoose
-  .connect(
-    process.env.MONGODB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DataBase is working");
   })
   .catch((err) => console.log("Database Error", err));
 
-app.listen(process.env.PORT_LOCAL || process.env.PORT, () => console.log("Server Run"));
+app.listen(process.env.PORT_LOCAL || process.env.PORT, () =>
+  console.log("Server Run")
+);
