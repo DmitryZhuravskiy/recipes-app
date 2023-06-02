68f46c84e4170d312da766454c6d4e50862efa74
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2023-06-02 18:20:21 +0300

    v

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy-Report-Only",
-    "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    "default-src '*'; script-src '*'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
   );
 
   next();
